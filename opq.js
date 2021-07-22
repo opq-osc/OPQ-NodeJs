6d8a3e39c568ef13d25656e9032f18c2a5c57b6c
XJL <xxxx@xxxx.com>
2021-07-22 10:32:02 +0800

    test

--- opq.js
+++ opq.js
@@ -1,6 +1,6 @@
 require('./src/main/text')
 require('./src/main/picture')
 require('./src/main/joinGroup')
-require('./src/main/Bili')
+require('./src/main/bili')
 require('./src/main/job')
 console.log("opq-nodejs正在启动。。。");
