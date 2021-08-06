0f6782582e8b95f833e433f2b54951283b32b020
XJL <xxxx@xxxx.com>
2021-08-06 10:26:19 +0800

    fix 星座运势

--- src/plugins/Constellation.js
+++ src/plugins/Constellation.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const axios = require('axios')
 const cheerio = require('cheerio')
 const Api = require('../SendMsg')
 const map = new Map([
@@ -26,54 +27,36 @@ let Constellation = {
             }
         }
         if (flag) {
-            https.get('https://www.xzw.com/fortune/' + map.get(keyWord) + '/', res => {
-                const { statusCode } = res
-                const contentType = res.headers['content-type']
-                let error;
-                if (statusCode !== 200) {
-                    error = new Error('请求失败\n' + `状态码: ${statusCode}`)
-                }
-                if (error) {
-                    console.error(error.message)
-                    // 消费响应数据来释放内存。
-                    res.resume()
-                    return
-                }
-                let html = ''
-                // 数据分段 只要接收数据就会触发data事件  chunk：每次接收的数据片段
-                res.on('data', (chunk) => {
-                    html += chunk.toString('utf-8')
+            axios.get("https://www.xzw.com/fortune/aries/",{
+                httpsAgent: new https.Agent({
+                  rejectUnauthorized: false
                 })
-                // 数据流传输完毕
-                res.on('end', () => {
-                    console.log('数据传输完毕！')
-                    try {
-                        let $ = cheerio.load(html)
-                        let value = []
-                        let key = []
-                        let title = ''
-                        $('#view > div.c_main > div.top > strong').each((index, el) => {
-                            title = ($(el).text().replace(/[\n\t]/g, ''))
-                        })
-                        $('#view > div.c_main > div.c_box > div.c_cont strong').each((index, el) => {
-                            key.push('⭐' + $(el).text().replace(/[\n\t]/g, '') + '⭐' + '\n')
-                        })
-                        $('#view > div.c_main > div.c_box > div.c_cont span').each((index, el) => {
-                            value.push($(el).text().replace(/[\n\t]/g, '') + '\n')
-                        })
-                        let content = ''
-                        for (let index in key) {
-                            content += key[index] + value[index]
-                        }
-                        console.log(title + content)
-                        Api.SendTextMsg(groupId, title + '\n' + content)
-                        // Api.SendPicMsgV2(groupId, title + '\n' + content)
-                    } catch (e) {
-                        console.error(e.message);
+              }).then(res =>{
+                const html = res.data
+                try {
+                    let $ = cheerio.load(html)
+                    let value = []
+                    let key = []
+                    let title = ''
+                    $('#view > div.c_main > div.top > strong').each((index, el) => {
+                        title = ($(el).text().replace(/[\n\t]/g, ''))
+                    })
+                    $('#view > div.c_main > div.c_box > div.c_cont strong').each((index, el) => {
+                        key.push('⭐' + $(el).text().replace(/[\n\t]/g, '') + '⭐' + '\n')
+                    })
+                    $('#view > div.c_main > div.c_box > div.c_cont span').each((index, el) => {
+                        value.push($(el).text().replace(/[\n\t]/g, '') + '\n')
+                    })
+                    let content = ''
+                    for (let index in key) {
+                        content += key[index] + value[index]
                     }
-                })
-            }).on('error', (e) => {
-                console.error(`出现错误: ${e.message}`);
+                    console.log(title + content)
+                    Api.SendTextMsg(groupId, title + '\n' + content)
+                    // Api.SendPicMsgV2(groupId, title + '\n' + content)
+                } catch (e) {
+                    console.error(e.message);
+                }
             })
         }
     }
