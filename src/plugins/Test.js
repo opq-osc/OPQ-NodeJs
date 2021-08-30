e75401c92d8c4d16fa1a4c134befc3240f2a0588
XJL <xxxx@xxxx.com>
2021-08-30 11:26:49 +0800

    update

--- src/plugins/Test.js
+++ src/plugins/Test.js
@@ -14,9 +14,10 @@ let Wordcloud = {
         const pixiv = new Pixiv();
         await pixiv.relogin();
 
-        // #pixiv:u:111
-        // #pixiv:p:12312
-        // #pixiv:b
+        // #pixiv:u:111 作者uid
+        // #pixiv:p:12312  插画pid
+        // #pixiv:b  收藏插画
+        // #pixiv:f  收藏画师
         const key = Content.split(':')
         if (key[1] == 'u') {
             uids = key[2].split(',');
@@ -28,6 +29,9 @@ let Wordcloud = {
         }
         if (key[1] == 'b') {
             await pixiv.downloadBookmark();
+        }
+        if (key[1] == 'f') {
+            await pixiv.downloadFollowAll(false, false);
         }
     }
 }
